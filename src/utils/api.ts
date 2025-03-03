de1a8934e1df3d66e47ff392565ed70654f9ef05
ilyas <ilyasozsarac@gmail.com>
2025-03-03 16:34:18 +0300

    Güncellemeler yapıldı

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_URL = process.env.NODE_ENV === 'development' 
   ? 'http://localhost:3001/api'
-  : 'https://ipimtr.onrender.com/api'; // Render.com URL'iniz
+  : 'https://ipimtr-backend.onrender.com/api'; // Render.com URL'iniz
 
 export const getIpInfo = async (ip?: string) => {
   const endpoint = ip ? `/ip/${ip}` : '/ip';
